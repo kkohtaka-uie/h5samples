2b0f9552dafa5e9a01e0837b71c9a6ad6c0a1eaf
Kazumasa Kohtaka <kkohtaka@uievolution.com>
2013-05-13 21:39:31 +0900

    add a swipe gesture sample

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+/*jslint indent: 2, node: true */
+
+(function () {
+
+  'use strict';
+
+  var path, lrSnippet, folderMount, middleware;
+
+  path = require('path');
+  lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+
+  folderMount = function (connect, point) {
+    return connect['static'](path.resolve(point));
+  };
+
+  middleware = function (connect, options) {
+    return [lrSnippet, folderMount(connect, options.base)];
+  };
+
+  module.exports = function (grunt) {
+
+    grunt.initConfig({
+      connect : {
+        main: {
+          options : {
+            port : 8888,
+            base : 'src',
+            middleware : middleware
+          }
+        }
+      },
+      regarde : {
+        html : {
+          files : [ 'src/**/*' ],
+          tasks : [ 'livereload' ]
+        }
+      }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-livereload');
+    grunt.loadNpmTasks('grunt-regarde');
+
+    grunt.registerTask(
+      'default',
+      [ 'livereload-start', 'connect', 'regarde' ]
+    );
+  };
+}());
+
